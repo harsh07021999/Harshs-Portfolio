3661cf76c7fede72e480e71ee843541db24388d3
Harsh Vardhan <harshfebruary@gmail.com>
2023-12-04 01:56:00 +0530

    initial commit

---
+++ src/lib/components/NavMenu/NavMenu.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    
+    let currentRoute = '/';
+    $: {
+        if($page){
+            currentRoute = $page.url.pathname;
+        }
+    }
+</script>
